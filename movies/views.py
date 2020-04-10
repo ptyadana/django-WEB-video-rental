4f26ee5e4a9a79841a8ad1682ed1c4b0cbcf0a06
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-04-10 16:41:41 +0800

    base template and app template

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Genre, Movie
 
 def index(request):
-    return HttpResponse("Hello")
+    #all objects
+    #SELECT * FROM movie_movie
+    movies = Movie.objects.all()
+    # output = ', '.join([movie.title for movie in movies])
+
+    #filter
+    #SELECT * FROM movie_movie WHERE released_year = 2000
+    # Movie.objects.filter(released_year=2000)
+
+    #specific object
+    # Movie.objects.get(id=1)
+
+    return render(request, 'movies/index.html', {'movies': movies})
