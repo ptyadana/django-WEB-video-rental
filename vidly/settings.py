7f1b1f3877db56abc4b8dbead1db81555fe2f58a
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-04-10 20:37:24 +0800

    add api and home

--- vidly/settings.py
+++ vidly/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movies',
+    'movies.apps.MoviesConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
--- vidly/urls.py
+++ vidly/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from movies import urls
+from api.models import MovieResource
+from . import views 
+
+movie_resource = MovieResource()
 
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
-    path('movies/', include('movies.urls'))
+    path('movies/', include('movies.urls')),
+    path('api/', include(movie_resource.urls)),
 ]
