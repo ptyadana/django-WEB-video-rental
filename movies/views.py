a0b4856cf3a2a71ae1c936adf658dc2649ea35fb
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-04-10 17:05:20 +0800

    add 404 error handling

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Genre, Movie
 
 def index(request):
@@ -18,5 +18,12 @@ def index(request):
     return render(request, 'movies/index.html', {'movies': movies})
 
 def detail(request, movie_id):
-    movie = Movie.objects.get(pk=movie_id)
+    # try:
+    #     movie = Movie.objects.get(id=movie_id)
+    #     return render(request, 'movies/detail.html',{'movie': movie})
+    # except Movie.DoesNotExist:
+    #     raise Http404()
+
+    movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'movies/detail.html',{'movie': movie})
+    
